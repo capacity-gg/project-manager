1e8b3810de1987e5cf321a417f8bace5c1eaabec
Patrick <patrick@capacity.tv>
2020-02-11 15:21:36 -0800

    Added starter pages for vue build

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>Welcome</h1>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld'
+
+export default {
+  components: {
+    HelloWorld // make the <HelloWorld> tag available
+  },
+  data: function () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style lang='scss'>
+</style>
