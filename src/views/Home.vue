cc87fb5bb48d1d596f7334287d80afd7ec73fb27
Patrick <patrick@capacity.tv>
2020-02-11 15:30:58 -0800

    Added FullCalendar plugin

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="home">
     <h1>Welcome</h1>
-    <HelloWorld></HelloWorld>
+    <FullCalendar defaultView="dayGridMonth" :plugins="calendarPlugins" />
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld'
+//import HelloWorld from '@/components/HelloWorld'
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
 export default {
   components: {
-    HelloWorld // make the <HelloWorld> tag available
+    //HelloWorld,
+    FullCalendar
   },
   data: function () {
-    return {}
+    return {
+      calendarPlugins: [ dayGridPlugin ]
+    }
   },
   methods: {}
 }
 </script>
 
 <style lang='scss'>
+
+@import '~@fullcalendar/core/main.css';
+@import '~@fullcalendar/daygrid/main.css';
+
 </style>
