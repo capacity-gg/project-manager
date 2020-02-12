ce020b126deefb47e7be5399d9d646f5d65bc7e7
Patrick <patrick@capacity.tv>
2020-02-11 16:34:42 -0800

    Added draggable interaction functionality and moment plugin.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="home">
     <h1>Welcome</h1>
-    <FullCalendar defaultView="dayGridMonth" :plugins="calendarPlugins" />
+    <FullCalendar 
+      defaultView="dayGridWeek" 
+      :titleFormat="title"
+      :header="calendarHeaders"
+      :plugins="calendarPlugins"
+      :editable="true"
+      :weekends="true"
+      :dragScroll="true"
+      :droppable="true"
+      :events="events"
+    />
   </div>
 </template>
 
 <script>
 //import HelloWorld from '@/components/HelloWorld'
 import FullCalendar from '@fullcalendar/vue'
+import momentPlugin from '@fullcalendar/moment';
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction';
 
 export default {
   components: {
@@ -17,7 +29,21 @@ export default {
   },
   data: function () {
     return {
-      calendarPlugins: [ dayGridPlugin ]
+      title: '{{MMM D}}',
+      calendarHeaders: {
+        left: 'prev, next',
+        center: '',
+        right: 'title'
+      },
+      calendarPlugins: [ 
+        dayGridPlugin,
+        interactionPlugin,
+        momentPlugin
+      ],
+      events: [
+        { title: 'event 1', date: '2020-02-14' },
+        { title: 'event 2', date: '2020-02-19' }
+      ]
     }
   },
   methods: {}
