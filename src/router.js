33d505802b4a0e5f2248e21866e87fd33bffc95a
Patrick <patrick@capacity.tv>
2020-02-26 16:55:07 -0800

    Changed project name to "project-manager".

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from './components/Home.vue'
 
 Vue.use(Router)
 
@@ -7,7 +8,7 @@ export default new Router({
   routes: [{
     path: '/',
     name: 'home',
-    component: () => import('./components/Home.vue')
+    component: Home
   }, {
     path: '/about',
     name: 'about',
