0a7f44cd35741d0f48c57f08ab2854c4c7ff9a69
Patrick <patrick@capacity.tv>
2020-03-06 15:27:36 -0800

    Added 404 page for any unidentified routes.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './components/home.vue'
+import NotFound from './components/notFound.vue'
 
 Vue.use(Router)
 
@@ -22,5 +23,8 @@ export default new Router({
     path: '/milestones',
     name: 'project_milestones',
     component: () => import('./components/milestones.vue')
+  }, { 
+    path: '*', 
+    component: NotFound 
   }]
 })
